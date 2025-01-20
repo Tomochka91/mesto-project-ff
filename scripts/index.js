322c855678f82024754c6f1bca720582f99de363
Tamara Tylik <tylik.tamara@yandex.ru>
2025-01-20 18:00:17 +0300

    feature: add functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
+function addCard(cardContent, deleteCardCallback) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = cardContent.link;
+  cardElement.querySelector(".card__title").textContent = cardContent.name;
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", deleteCardCallback);
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+
+  eventTarget.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((element) => {
+  placesList.append(addCard(element, deleteCard));
+});
